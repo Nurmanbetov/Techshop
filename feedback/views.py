c648eeb4771b5e70d5f2855bf5afe764aeb2d032
nurmanbetov594 <nurmanbetov594@gmail.com>
2020-10-08 15:42:43 +0600

    added feedback

--- feedback/views.py
+++ feedback/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.views.decorators.http import require_POST
+from .forms import FeedBackForm
 
-# Create your views here.
+
+@require_POST
+def feedback(request):
+    context = {}
+    form = FeedBackForm(request.POST, require.FILES)
+    if forms.is_valid():
+        feedback = form.save()
+        if request.is_authenticated:
+            feedback.user = request.user
+            feedback.save()
+            
+        context["message"] = "Ваше обрашение принято, спасибо!"
+        context["type"] = "success"
+        return render(request, "core/message.html", context)
+
+    context["message"] = "Форма заполнено не верно!"
+    context["type"] = "warnings"
+    return render(request, "core/message.html", context)
+
+    
+
+
